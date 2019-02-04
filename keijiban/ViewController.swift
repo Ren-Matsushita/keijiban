baffc46c50cbfde330934b14998aab91ada1619c
Ren Matsushita <ren1725m@gmail.com>
2019-02-04 13:03:50 +0900

    Initial Commit

---
+++ keijiban/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  keijiban
+//
+//  Created by Ren Matsushita on 2019/02/04.
+//  Copyright © 2019 Ren Matsushita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
